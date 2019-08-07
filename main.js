0cf746b76ee233336f50d36a50717f5b93216126
voidbringer01 <voidbringer01@touka>
2019-08-07 19:41:08 +0200

    Completed

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow} = require('electron')
+
+
+let window 
+function createWindow()
+{
+    window = new BrowserWindow({
+        width:800,
+        height:800,
+        webPreferences:{
+            nodeIntegration: true
+        }
+    })
+
+
+
+    window.loadFile('index.html')
+
+    window.webContents.openDevTools()
+
+    window.on('closed',()=>{
+        window=null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed',()=>{
+    if(process.platform!=='darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate',()=>{
+    if(win===null){
+        createWindow()
+    }
+})  
